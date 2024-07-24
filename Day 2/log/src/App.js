27d1d347eea6b01e6b70e8c5dbaaec3bfcb1400c
Rahunandan <150135359+Rahunandan@users.noreply.github.com>
2024-07-24 10:19:57 +0530

    Add files via upload

---
+++ Day 2/log/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Auth from './Auth';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Auth/>
+    </div>
+  );
+}
+
+export default App;
